ae7f736e4807f80ffeeeca6e62005a6640145570
PA314159 <pa314159@users.sf.net>
2009-05-04 14:59:28 +0000

    The build system has been migrated to Maven.

--- src/pi/eclipse/cle/util/WidgetListener.java
+++
@@ -1,9 +0,0 @@
-package pi.eclipse.cle.util;
-
-/**
- * @author <a href="mailto:pa314159&#64;sf.net">Paπ &lt;pa314159&#64;sf.net&gt;</a>
- */
-public interface WidgetListener
-{
-	void updateContainer( String status );
-}
